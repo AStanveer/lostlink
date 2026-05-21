dad9398819cc8c253e06d5e2e73b5d6d08b88c69
wj <151712052+ThangWeiJie@users.noreply.github.com>
2026-05-21 10:23:41 +0800

    Added props to display items

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -1,15 +1,20 @@
 <script setup>
+defineProps({
+  /** @type Item */
+  item: Object
+})
 
+const PLACEHOLDER_IMAGE_URL = "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
 </script>
 
 <template>
   <div class="shadow-lg m-4 outline-black/5 hover:shadow-xl hover:-translate-y-2">
     <div>
-      <img src="https://www.stockvault.net/data/2011/02/06/117454/thumb16.jpg" class="w-full h-56 object-cover object-top" >
+      <img :src="item.image_path ? item.image_path : PLACEHOLDER_IMAGE_URL" class="w-full h-56 object-cover object-top" alt="Posted image">
     </div>
     <div class="flex flex-col p-4 gap-3">
-      <span class="text-xl font-medium text-black">Title here</span>
-      <p class="truncate">Some description here Some description here Some description here</p>
+      <span class="text-xl font-medium text-black">{{ item.title }}</span>
+      <p class="truncate">{{ item.description }}</p>
       <a class="self-end underline" href="#">Link here</a>
     </div>
   </div>
